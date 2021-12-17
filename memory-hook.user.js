4d5bd22a20aaeb343fb2711faa18733ce7445ddf
Nulled <63370961+Nul-led@users.noreply.github.com>
2021-12-17 22:19:21 +0100

    Create memory-hook.user.js

---
+++ memory-hook.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        Diep Memory Hook
+// @author      Nulled
+// @description Hooks into the games memory and allows accessing it
+// @match       https://diep.io/*
+// @version     1.0.0
+// @run-at      document-load
+// @grant       none
+// ==/UserScript==
+
+const proxy = window.WebAssembly.Memory
+// function for execution context
+window.WebAssembly.Memory = function(arg) {
+    const memory = new proxy(arg);
+    window.diepMemory = {
+        buffer: memory.buffer,
+        HEAP8: new Int8Array(memory.buffer),
+        HEAP16: new Int16Array(memory.buffer),
+        HEAP32: new Int32Array(memory.buffer),
+        HEAPF32: new Float32Array(memory.buffer),
+        HEAPF64: new Float64Array(memory.buffer),
+        HEAPU8: new Uint8Array(memory.buffer),
+        HEAPU16: new Uint16Array(memory.buffer),
+        HEAPU32: new Uint32Array(memory.buffer)
+    }
+    return memory;
+}
