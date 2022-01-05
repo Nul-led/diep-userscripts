200fe13e307dce447d69427e46445332163b0d3e
Nulled <63370961+Nul-led@users.noreply.github.com>
2022-01-05 15:32:48 +0100

    no more build in the html

--- rainbow.user.js
+++ rainbow.user.js
@@ -14,12 +14,6 @@ if(!window.localStorage.rainbowInterval) window.localStorage.rainbowInterval = 1
 // TODO: change each entities color seperately
 const applyToAllGroups = (root, offset, value, mem) => { for(let at = mem.HEAP32[root >> 2]; at < mem.HEAP32[root + 4 >> 2]; at = at + 4) mem.HEAP32[mem.HEAP32[at >> 2] + offset >> 2] = value; };
 (async () => {
-    const wait = cb => {
-        if(!window.hasOwnProperty('Hook') && !window.hasOwnProperty('diepMemory')) return setTimeout(wait, 10, cb);
-        if(!localStorage.scriptConfig || (/(?!build_)[0-9a-f]{40}(?=\.wasm\.js)/.exec(document.body.innerHTML) || [false])[0] !== JSON.parse(window.localStorage.scriptConfig).build) return setTimeout(wait, 10, cb);
-        cb();
-    }
-    await new Promise(wait);
     const root = JSON.parse(localStorage.scriptConfig).constantPointers.rootFieldGroup + 11 * 12, offset = JSON.parse(localStorage.scriptConfig).offsets.color;
     if(!window.hasOwnProperty('Hook')) setInterval(() => applyToAllGroups(root, offset, Math.floor(Math.random() * 18), window.diepMemory), 50 * parseInt(window.localStorage.rainbowInterval));
     else {
