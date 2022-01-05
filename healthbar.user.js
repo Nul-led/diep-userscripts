7139feb276354357123632acd5e647da6d67c982
Nulled <63370961+Nul-led@users.noreply.github.com>
2022-01-05 15:32:23 +0100

    no more build in the html

--- healthbar.user.js
+++ healthbar.user.js
@@ -9,12 +9,6 @@
 // ==/UserScript==
 const applyToAllGroups = (root, offset, value, mem) => { for(let at = mem.HEAP32[root >> 2]; at < mem.HEAP32[root + 4 >> 2]; at = at + 4) mem.HEAP32[mem.HEAP32[at >> 2] + offset >> 2] = value; };
 (async () => {
-    const wait = cb => {
-        if(!window.hasOwnProperty('Hook') && !window.hasOwnProperty('diepMemory')) return setTimeout(wait, 10, cb);
-        if(!localStorage.scriptConfig || (/(?!build_)[0-9a-f]{40}(?=\.wasm\.js)/.exec(document.body.innerHTML) || [false])[0] !== JSON.parse(window.localStorage.scriptConfig).build) return setTimeout(wait, 10, cb);
-        cb();
-    }
-    await new Promise(wait);
     const root = JSON.parse(localStorage.scriptConfig).constantPointers.rootFieldGroup + 4 * 12, offset = JSON.parse(localStorage.scriptConfig).offsets.healthbar;
     if(!window.hasOwnProperty('Hook')) setInterval(() => applyToAllGroups(root, offset, 0, window.diepMemory), 50); // equal to approximately one gametick
     else window.Hook.addEventListener('clientbound', ({ data }) => { if(data[0] === 0) applyToAllGroups(root, offset, 0, window.Hook.Module); });
